05e7a77aafa8f2276602e4ec954ac2ba84417b5b
Prasanta Mondal <mondalprasanta801@gmail.com>
2024-03-01 11:09:12 +0530

    gitignore changes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const Patient = require("./models/Patient.js");
 const qr = require("qrcode");
+require("dotenv").config();
 
 const app = express();
 
@@ -14,10 +15,11 @@ const PORT = process.env.PORT || 5000;
 // MongoDB connection
 app.use(cors());
 app.use(express.json());
-mongoose.connect(
-  "mongodb+srv://0000:0000@cluster0.idxidv0.mongodb.net/?retryWrites=true&w=majority&appName=cluster0",
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+console.log(process.env.DBmongo);
+mongoose.connect(process.env.DBmongo, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
